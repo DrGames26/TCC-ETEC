e66d67e6f0b4720ab343da1e9e59cc07a1e6cc2a
DrGames26 <felipechiav@gmail.com>
2024-11-06 09:37:29 -0300

    Minha estante

--- src/app/components/estante/estante.component.ts
+++ src/app/components/estante/estante.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LivroService, Livro } from 'src/app/services/livro.service';
+import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,20 +10,30 @@ import { Router } from '@angular/router';
 })
 export class EstanteComponent implements OnInit {
   livros: Livro[] = [];
-  user = { name: 'user.name' };
+  userName: string | null = null;
 
-  constructor(private livroService: LivroService, private router: Router) {}
+  constructor(
+    private livroService: LivroService,
+    private authService: AuthService, // Injeta o AuthService
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.carregarLivrosDaEstante();
+    const user = this.authService.getUser(); // Obtém o usuário logado
+    if (user) {
+      this.userName = user.name; // Atribui o nome do usuário logado
+      this.carregarLivrosDaEstante();
+    } else {
+      console.error("Usuário não autenticado.");
+    }
   }
 
   carregarLivrosDaEstante(): void {
     this.livroService.getLivros().subscribe(
       (data) => {
-        // Filtra os livros publicados pelo usuário logado e ordena por ID
+        // Filtra os livros para exibir apenas os publicados pelo usuário logado
         this.livros = data
-          .filter((livro: Livro) => livro.usuarioPublicador === this.user.name)
+          .filter((livro: Livro) => livro.usuarioPublicador === this.userName)
           .sort((a: Livro, b: Livro) => (b.id || 0) - (a.id || 0));
       },
       (error) => console.error('Erro ao carregar livros da estante:', error)
