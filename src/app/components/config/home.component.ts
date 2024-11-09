5e452e1a134298bf28ed09ed0c9a5a724da0b638
felipe <felipechiav@gmail.com>
2024-11-09 18:10:41 -0300

    estilização da home e livros

--- src/app/components/config/home.component.ts
+++ src/app/components/config/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { LivroService } from '../../services/livro.service'; // Ajuste o caminho conforme necessário
+import { LivroService } from '../../services/livro.service'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +11,17 @@ export class HomeComponent implements OnInit {
 
   livros: any[] = [];  // Define a variável para armazenar os livros
 
-  constructor(private livroService: LivroService) {}
+  constructor(private livroService: LivroService, private router: Router) {}
 
   ngOnInit(): void {
     this.livroService.getLivros().subscribe((data: any) => {
       this.livros = data;
       console.log(this.livros);
     });
+  }
+
+  // Método para navegar para a página de detalhes do livro
+  navegarParaDetalhes(livroId: number): void {
+    this.router.navigate(['/livro', livroId]); // Altere a rota conforme sua configuração
   }
 }
