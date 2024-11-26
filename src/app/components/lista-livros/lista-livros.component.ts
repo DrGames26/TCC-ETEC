9298e2eb7eb5079ca2d3bc2dfc0d5a0f4e36aa2a
unknown <felipe.chiav@gmail.com>
2024-11-26 17:17:51 -0300

    arquivos locais

--- src/app/components/lista-livros/lista-livros.component.ts
+++ src/app/components/lista-livros/lista-livros.component.ts
@@ -10,7 +10,7 @@ import { LivroService, Livro } from 'src/app/services/livro.service';
 export class ListaLivrosComponent implements OnInit {
   livros: Livro[] = [];
 
-  constructor(private livroService: LivroService, private router: Router) {} // Adiciona Router ao construtor
+  constructor(private livroService: LivroService, private router: Router) {}
 
   ngOnInit(): void {
     this.carregarLivros();
@@ -19,11 +19,28 @@ export class ListaLivrosComponent implements OnInit {
   carregarLivros(): void {
     this.livroService.getLivros().subscribe(
       (data) => {
+        // Converte o byte[] para base64
+        this.livros = data.map((livro: Livro) => {
+          if (livro.picture) {
+            livro.picture = this.convertToBase64(livro.picture);
+          }
+          return livro;
+        });
+
         // Ordena os livros por ID do maior para o menor
-        this.livros = data.sort((a: Livro, b: Livro) => (b.id || 0) - (a.id || 0));
+        this.livros.sort((a: Livro, b: Livro) => (b.id || 0) - (a.id || 0));
       },
       (error) => console.error('Erro ao carregar livros:', error)
     );
+  }
+
+  convertToBase64(byteArray: any): string {
+    let binaryString = '';
+    const bytes = new Uint8Array(byteArray);
+    bytes.forEach((byte) => {
+      binaryString += String.fromCharCode(byte);
+    });
+    return btoa(binaryString);
   }
 
   navegarParaDetalhes(id: number): void {
