f58690e678dab5a43e65763cb1c9441d873e3a04
unknown <felipe.chiav@gmail.com>
2024-11-28 14:37:57 -0300

    estilização e ajustes

--- src/app/components/estante/estante.component.ts
+++ src/app/components/estante/estante.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LivroService } from '../../services/livro.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Livro } from '../../services/livro.service';
-import { AuthService } from '../../services/auth.service';
+import { LivroService, Livro } from 'src/app/services/livro.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-estante',
@@ -11,61 +10,37 @@ import { AuthService } from '../../services/auth.service';
 })
 export class EstanteComponent implements OnInit {
   livros: Livro[] = [];
-  user: any = null; // Alterado para user em vez de userName
-  bookToEdit: Livro | null = null;
-  livroToDelete: Livro | null = null;
+  userName: string | null = null;
 
   constructor(
     private livroService: LivroService,
-    private modalService: NgbModal,
-    private authService: AuthService
+    private authService: AuthService, // Injeta o AuthService
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.loadLivros();
-    this.user = this.authService.getUser(); // Alterado para pegar o usuário completo
-  }
-
-  private loadLivros() {
-    if (this.user) {
-      this.livroService.getBooksByUsuarioPublicador(this.user.name).subscribe((livros) => {
-        this.livros = livros;
-      });
+    const user = this.authService.getUser(); // Obtém o usuário logado
+    if (user) {
+      this.userName = user.name; // Atribui o nome do usuário logado
+      this.carregarLivrosDaEstante();
+    } else {
+      console.error("Usuário não autenticado.");
     }
   }
 
-  navegarParaDetalhes(livroId: number) {
-    console.log('Ver detalhes do livro com ID:', livroId);
-  }
-
-  openEditBookModal(livro: Livro, content: any) {
-    this.bookToEdit = { ...livro };
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
-  }
-
-  openDeleteBookModal(livro: Livro, content: any) {
-    this.livroToDelete = livro;
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
-      (result) => {
-        if (result === 'confirm' && this.livroToDelete) {
-          this.deleteBook(this.livroToDelete.id);
-        }
+  carregarLivrosDaEstante(): void {
+    this.livroService.getLivros().subscribe(
+      (data) => {
+        // Filtra os livros para exibir apenas os publicados pelo usuário logado
+        this.livros = data
+          .filter((livro: Livro) => livro.usuarioPublicador === this.userName)
+          .sort((a: Livro, b: Livro) => (b.id || 0) - (a.id || 0));
       },
-      () => {}
+      (error) => console.error('Erro ao carregar livros da estante:', error)
     );
   }
 
-  deleteBook(bookId: number) {
-    this.livroService.deleteBook(bookId).subscribe(() => {
-      this.livros = this.livros.filter(livro => livro.id !== bookId);
-    });
-  }
-
-  saveBookChanges() {
-    if (this.bookToEdit) {
-      this.livroService.updateBook(this.bookToEdit).subscribe(() => {
-        this.loadLivros();
-      });
-    }
+  navegarParaDetalhes(id: number): void {
+    this.router.navigate(['/livro', id]);
   }
 }
