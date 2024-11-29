e252eda062e6deab3cfb7281d34b45beadb785fd
unknown <felipe.chiav@gmail.com>
2024-11-29 13:36:44 -0300

    voltei

--- src/app/components/estante/estante.component.ts
+++ src/app/components/estante/estante.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Livro, LivroService } from 'src/app/services/livro.service';
+import { AuthService } from 'src/app/services/auth.service'; 
 
 @Component({
   selector: 'app-estante',
@@ -16,14 +17,22 @@ export class EstanteComponent implements OnInit {
     description: '',
   };
 
-  constructor(private livroService: LivroService) {}
+  constructor(
+    private livroService: LivroService,
+    private authService: AuthService 
+  ) {}
 
   ngOnInit(): void {
     this.carregarLivros();
   }
 
   carregarLivros(): void {
-    this.livroService.getLivros().subscribe((data) => (this.livros = data));
+    this.livroService.getLivros().subscribe((data) => {
+      const usuarioLogado = this.authService.getUser(); // Obtém o usuário logado
+      if (usuarioLogado) {
+        this.livros = data.filter((livro) => livro.usuarioPublicador === usuarioLogado.email);
+      }
+    });
   }
 
   editarLivro(livro: Livro): void {
