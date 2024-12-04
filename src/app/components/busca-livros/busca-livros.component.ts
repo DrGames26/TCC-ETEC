fa71fa1af91ea170b23b2befb1998bfce9996545
felipe <felipechiav@gmail.com>
2024-12-04 19:32:15 -0300

    pesquisa vai

--- src/app/components/busca-livros/busca-livros.component.ts
+++ src/app/components/busca-livros/busca-livros.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LivroService, Livro } from '../../services/livro.service';
-import { Router } from '@angular/router';  // Importa o Router para navegação
+import { Router } from '@angular/router'; // Importa o Router para navegação
 
 @Component({
   selector: 'app-busca-livros',
@@ -8,9 +8,9 @@ import { Router } from '@angular/router';  // Importa o Router para navegação
   styleUrls: ['./busca-livros.component.css']
 })
 export class BuscaLivrosComponent implements OnInit {
-  query: string = '';  // A variável que armazena o texto da pesquisa
-  livros: Livro[] = [];  // Lista de livros a serem exibidos
-  isLoading: boolean = false;  // Flag para mostrar carregamento
+  query: string = ''; // A variável que armazena o texto da pesquisa
+  livros: Livro[] = []; // Lista de livros a serem exibidos
+  isLoading: boolean = false; // Flag para mostrar carregamento
 
   constructor(private livroService: LivroService, private router: Router) {}
 
@@ -21,30 +21,29 @@ export class BuscaLivrosComponent implements OnInit {
   // Método para buscar livros
   buscarLivros(): void {
     if (this.query.trim() === '') {
-      this.livros = [];  // Limpa os resultados se a busca estiver vazia
+      this.livros = []; // Limpa os resultados se a busca estiver vazia
       return;
     }
 
-    this.isLoading = true;  // Ativa o carregamento enquanto busca os dados
-    this.livroService.getLivros()
-      .subscribe(
-        (livros: Livro[]) => {
-          this.livros = livros.filter(livro => 
+    this.isLoading = true; // Ativa o carregamento enquanto busca os dados
+    this.livroService.getLivros().subscribe(
+      (livros: Livro[]) => {
+        this.livros = livros.filter(
+          livro =>
             livro.name.toLowerCase().includes(this.query.toLowerCase()) ||
             livro.author.toLowerCase().includes(this.query.toLowerCase())
-          );
-          this.isLoading = false;  // Desativa o carregamento após os dados serem recebidos
-        },
-        (error) => {
-          console.error('Erro ao buscar livros:', error);
-          this.isLoading = false;  // Desativa o carregamento em caso de erro
-        }
-      );
+        );
+        this.isLoading = false; // Desativa o carregamento após os dados serem recebidos
+      },
+      error => {
+        console.error('Erro ao buscar livros:', error);
+        this.isLoading = false; // Desativa o carregamento em caso de erro
+      }
+    );
   }
 
   // Método para navegar para a página de detalhes do livro
   navegarParaDetalhes(livroId: number): void {
     this.router.navigate([`/livro/${livroId}`]);
   }
-  
 }
